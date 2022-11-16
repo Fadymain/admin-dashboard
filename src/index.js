cb6c81195645fb1f4a1448aa4d13da37f17f86eb
Adetola <adetolafadairo@gmail.com>
2022-11-16 12:55:29 -0800

    updating ContextProvider path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {ContextProvider} from './contexts';
+import {ContextProvider} from './context/ContextProvider';
 
 ReactDOM.render(
     <ContextProvider>
