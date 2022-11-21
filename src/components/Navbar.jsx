04054c18eea8efc50ba644f101165558a091750e
Adetola <adetolafadairo@gmail.com>
2022-11-21 12:01:05 -0800

    implementing NavBar to hide or display

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,13 @@ import { useStateContext } from '../context/ContextProvider';
 
 const NavButton = ({title, customFunc, icon, color, dotColor}) => (
   <TooltipComponent content={title} position='BottomCenter'>
-    <button></button>
+    <button type='button' onClick={customFunc} style={{color}} 
+    className='relative text-xl rounded-full p-3 hover:bg-light-gray'
+    >
+      <span style={{background: dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'>
+        {icon}
+      </span>
+    </button>
   </TooltipComponent>
 );
 
@@ -21,7 +27,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
-      <NavButton />
+      <NavButton title='Menu' customFunc={() => setActiveMenu((prevActivemenu) => !prevActivemenu)} color='blue' icon={<AiOutlineMenu />} />
     </div>
   )
 }
