c04230cbcf7d3674103480279ee53a649b5d3278
Adetola <adetolafadairo@gmail.com>
2022-12-06 18:39:01 -0800

    using Button component

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -10,7 +10,21 @@ const Ecommerce = () => {
     <div className='mt-12'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
         <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repat bg-cover bg-center'>
-
+          <div className='flex justify-between items-center'>
+            <div>
+              <p className='fon-bold text-gray-400'>Earnings</p>
+              <p className='text-2xl'>$60,050.30</p>
+            </div>
+          </div>
+          <div className='mt-6'>
+            <Button
+              color='white'
+              bgColor='blue'
+              text='Download'
+              borderRadius='10px'
+              size='md'
+            />
+          </div>
         </div>
       </div>
     </div>
