c9d9d21849ac3cb7f51a56462e7a6db7262df66d
Adetola <adetolafadairo@gmail.com>
2022-12-28 12:20:03 -0800

    passing currentColor state to color

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -21,12 +21,14 @@ export const ContextProvider = ({children}) => {
         setCurrentMode(e.target.value);
 
         localStorage.setItem('themeMode', e.target.value);
+        setThemeSettings(false);
     }
 
-    const setColor = (e) => {
-        setCurrentColor(e.target.value);
+    const setColor = (color) => {
+        setCurrentColor(color);
 
-        localStorage.setItem('colorMode', e.target.value);
+        localStorage.setItem('colorMode', color);
+        setThemeSettings(false);
     }
 
     const handleClick = (clicked) => {
@@ -44,8 +46,8 @@ export const ContextProvider = ({children}) => {
             handleClick,
             screenSize, setScreenSize,
             currentColor, currentMode,
-            setCurrentColor, setCurrentMode,
-            themeSettings, setThemeSettings
+            themeSettings, setThemeSettings,
+            setMode, setColor
         }}
         >
             {children}
