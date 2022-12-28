32551fefdc79dd3e7ef64501e82b47a413464d94
Adetola <adetolafadairo@gmail.com>
2022-12-28 14:00:13 -0800

    importing and using currentMode context

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
 import { lineCustomSeries, LinePrimaryYAxis, LinePrimaryXAxis } from '../../data/dummy';
+import { useStateContext } from '../../context/ContextProvider';
 
 const LineChart = () => {
+
+  const {currentMode} = useStateContext();
+
   return (
     <ChartComponent
       id='line-chart'
@@ -11,6 +15,7 @@ const LineChart = () => {
       primaryYAxis={LinePrimaryYAxis}
       chartArea={{ border: { width: 0 }}}
       tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
     >
       <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
       <SeriesCollectionDirective>
